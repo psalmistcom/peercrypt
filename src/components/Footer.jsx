359affe5d576848c87f3bf18df7ffda5d608fb56
psalmistcom <dadewale9@gmail.com>
2024-09-25 15:19:16 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 import { footernavItems } from "../constants";
 const Footer = () => {
   return (
-    <footer className="relative mt-10 px-4 pt-10">
+    <footer className="flex flex-col mt-10 px-4 pt-10 text-gray-500">
       <nav
         aria-label="Footer Navigation"
         className="mx-auto mb-10 flex max-w-lg flex-col gap-10 text-center sm:flex-row sm:text-left"
       >
         {footernavItems.map((item, index) => (
-          <a key={index} href={item.href} className="font-normal text-white">
+          <a key={index} href={item.href} className="font-normal">
             {item.label}
           </a>
         ))}
       </nav>
-      <p className="mb-5 text-center text-gray-300">
+      <p className="mb-5 text-center">
         © 2024 HackaBlock | All Rights Reserved
       </p>
     </footer>
